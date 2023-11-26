f4b3787b5e5cfbfa377eb543ad474d045c8e9033
ethan <ethan.fifle@gmail.com>
2023-11-26 09:57:05 -0500

    Fragment update

--- main.js
+++ main.js
@@ -36,9 +36,6 @@ var eye;
 var at = vec3(0.0, 0.0, 0.0);
 var up = vec3(0.0, 1.0, 0.0);
 
-var RX = 0 ;
-var RY = 0 ;
-var RZ = 0 ;
 
 var MS = [] ; // The modeling matrix stack
 var TIME = 0.0 ; // Realtime
@@ -266,21 +263,6 @@ window.onload = function init() {
     setColor(materialDiffuse) ;
 
 
-
-    // set the callbacks for the UI elements
-    document.getElementById("sliderXi").oninput = function() {
-        RX = this.value ;
-        window.requestAnimFrame(render);
-    };
-    document.getElementById("sliderYi").oninput = function() {
-        RY = this.value;
-        window.requestAnimFrame(render);
-    };
-    document.getElementById("sliderZi").oninput = function() {
-        RZ =  this.value;
-        window.requestAnimFrame(render);
-    };
-
     document.getElementById("animToggleButton").onclick = function() {
         if( animFlag ) {
             animFlag = false;
@@ -296,12 +278,6 @@ window.onload = function init() {
         toggleTextures() ;
         window.requestAnimFrame(render);
     };
-
-    var controller = new CameraController(canvas);
-    controller.onchange = function(xRot,yRot) {
-        RX = xRot ;
-        RY = yRot ;
-        window.requestAnimFrame(render); };
 
     // load and initialize the textures
     initTextures() ;
@@ -427,11 +403,6 @@ function render() {
     // initialize the modeling matrix stack
     MS= [] ;
     modelMatrix = mat4() ;
-
-    // apply the slider rotations
-    gRotate(RZ,0,0,1) ;
-    gRotate(RY,0,1,0) ;
-    gRotate(RX,1,0,0) ;
 
     // send all the matrices to the shaders
     setAllMatrices() ;
@@ -1798,57 +1769,7 @@ function render() {
 //   controller.onchange = function(xRot, yRot) { ... };
 //
 // The view matrix is computed elsewhere.
-function CameraController(element) {
-    var controller = this;
-    this.onchange = null;
-    this.xRot = 0;
-    this.yRot = 0;
-    this.scaleFactor = 3.0;
-    this.dragging = false;
-    this.curX = 0;
-    this.curY = 0;
 
-    // Assign a mouse down handler to the HTML element.
-    element.onmousedown = function(ev) {
-        controller.dragging = true;
-        controller.curX = ev.clientX;
-        controller.curY = ev.clientY;
-    };
-
-    // Assign a mouse up handler to the HTML element.
-    element.onmouseup = function(ev) {
-        controller.dragging = false;
-    };
-
-    // Assign a mouse move handler to the HTML element.
-    element.onmousemove = function(ev) {
-        if (controller.dragging) {
-            // Determine how far we have moved since the last mouse move
-            // event.
-            var curX = ev.clientX;
-            var curY = ev.clientY;
-            var deltaX = (controller.curX - curX) / controller.scaleFactor;
-            var deltaY = (controller.curY - curY) / controller.scaleFactor;
-            controller.curX = curX;
-            controller.curY = curY;
-            // Update the X and Y rotation angles based on the mouse motion.
-            controller.yRot = (controller.yRot + deltaX) % 360;
-            controller.xRot = (controller.xRot + deltaY);
-            // Clamp the X rotation to prevent the camera from going upside
-            // down.
-            if (controller.xRot < -90) {
-                controller.xRot = -90;
-            } else if (controller.xRot > 90) {
-                controller.xRot = 90;
-            }
-            // Send the onchange event to any listener.
-            if (controller.onchange != null) {
-                controller.onchange(controller.xRot, controller.yRot);
-            }
-        }
-    };
-
-}
 
 
 
